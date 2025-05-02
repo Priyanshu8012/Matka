2e5d570fd4538ef2507f5ba0e22064823efa4d46
Priyanshu8012 <141941989+Priyanshu8012@users.noreply.github.com>
2025-05-02 17:16:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import DpBossClone from "./components/Dpboss";
+
+import './index.css';
+
+function App() {
+  return (
+    <>
+      <DpBossClone />
+     
+    </>
+  );
+}
+
+export default App;
